7dc1f29b3d582b07d8497eb100e000a67f2a4c0f
Victorina Gergen <victorig@student.21-school.ru>
2024-08-24 17:20:11 +0700

    task_2-fix

--- src/task_2.py
+++ src/task_2.py
@@ -13,28 +13,13 @@ def number_of_differences(lst1: List[Union[int, str, bool]], lst2: List[Union[in
     return count
 
 
-def calculate_area(name: str, l: Tuple[float, ...]) -> Optional[float]:
-    if name == 'rectangle' and isinstance(name, str) and len(l) == 2 and isinstance(l[0],
-                                                                                    float) and isinstance(
-        l[1], float) and isinstance(l, tuple):
-        return round(l[0] * l[1], 2)
-    elif name == 'circle' and isinstance(name, str) and len(l) == 1 and isinstance(l[0],
-                                                                                   float) and isinstance(l, tuple):
-        return round(3.14159 * l[0] * l[0], 2)
+def calculate_area(name: str, l: Union[Tuple[float, ...], float]) -> Optional[float]:
+    if name == 'rectangle' and isinstance(l, tuple):
+        if isinstance(name, str) and len(l) == 2 and isinstance(l[0],float) and isinstance(l[1], float) :
+            return round(l[0] * l[1], 2)
+        else:
+            return None
+    elif name == 'circle' and isinstance(name, str) and isinstance(l, float) and isinstance(l, float):
+        return round(3.14159 * l * l, 2)
     else:
         return None
-
-
-# def main():
-#     name = "rectangle"
-#     l = [1.0, 2.0]
-#     print(calculate_area(name, l))
-#     name = "circle"
-#     l = (1.0,)
-#     print(calculate_area(name, l))
-#     print(number_of_differences([1, 2, 3.0, 4.0, 5.0, True], [True, "rt", 2.0, False, "wer", 1, 5.0]))
-#     print(number_of_differences([1, 2, 3.0, 4.0, 5.0, 6, 7], [2, 6, 7]))
-#
-#
-# if __name__ == '__main__':
-#     main()
